f8ef1bc597d68d93c8b85ca3b262382d96dff109
Miriam Espino <miriamespino@Miriams-MacBook-Pro.local>
2026-01-11 17:34:06 -0500

    client: simplify App.tsx to route only to HomePage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import HomePage from './pages/HomePage'
-import ElementsPage from './pages/ElementsPage'
 import Footer from './components/Footer'
 import './App.css'
 
@@ -12,9 +11,6 @@ export default function App(){
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage/>} />
-        <Route path="/elements" element={<ElementsPage/>} />
-        <Route path="/projects" element={<HomePage/>} />
-        <Route path="/art" element={<HomePage/>} />
       </Routes>
       <Footer />
     </BrowserRouter>
